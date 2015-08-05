02577ad3a600326799f7fab34bf437e376089af3
Manu343726 <Manu343726@gmail.com>
2015-08-05 22:35:05 +0200

    Make sid_hash() work with VS2015 RTM. Update current status on readme

--- ctti/detail/hash.hpp
+++ ctti/detail/hash.hpp
@@ -17,7 +17,11 @@ namespace ctti
         // FNV-1a 64 bit hash
         constexpr hash_t sid_hash(std::size_t n, const char *str, hash_t hash = fnv_basis)
         {
-            return n > 0 ? sid_hash(n - 1, str + 1, (hash ^ *str) * fnv_prime) : hash;
+#if _MSC_VER
+            return *str ? sid_hash(n - 1, str + 1, (hash ^ *str) * fnv_prime) : hash;
+#else
+			return n > 0 ? sid_hash(n - 1, str + 1, (hash ^ *str) * fnv_prime) : hash;
+#endif
         }
     }
 }
