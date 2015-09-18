f54083e902f4b74098f1246bc383f743dedb22c0
Manu343726 <Manu343726@gmail.com>
2015-09-19 01:39:17 +0200

    CMake-based project settings. Closes #7

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,10 @@
-#include "type_id.hpp"
+#include "ctti/type_id.hpp"
 #include <iostream>
 
 int main()
 {
     static_assert(ctti::detail::make_array("hello").size() != ctti::detail::make_array("?").size(), "???");
 
-	std::cout << ctti::type_id(std::cout).name() << "\n";
+	std::cout << ctti::type_id(std::cout).name() << "!!!\n";
+	std::cin.get();
 }
