dbe6045d440b608142c056977ec6dc28859c26e6
Manuel Sanchez <Manu343726@gmail.com>
2017-11-13 08:48:03 +0000

    Extra param for ctti::mapping() for user defined value mapping

--- include/ctti/map.hpp
+++ include/ctti/map.hpp
@@ -6,34 +6,55 @@
 namespace ctti
 {
 
+struct default_symbol_mapping_function
+{
+    constexpr default_symbol_mapping_function() = default;
+
+    template<typename Source, typename SourceSymbol, typename Sink, typename SinkSymbol>
+    constexpr void operator()(const Source& source, SourceSymbol, Sink& sink, SinkSymbol) const
+    {
+        ctti::get_member_value<SinkSymbol>(sink) = ctti::get_member_value<SourceSymbol>(source);
+    }
+};
+
+template<typename SourceSymbol, typename SinkSymbol, typename Source, typename Sink, typename Function>
+constexpr void map(const Source& source, Sink& sink, const Function& function)
+{
+    function(source, SourceSymbol(), sink, SinkSymbol());
+}
+
 template<typename SourceSymbol, typename SinkSymbol, typename Source, typename Sink>
 constexpr void map(const Source& source, Sink& sink)
 {
-    ctti::get_member_value<SinkSymbol>(sink) = ctti::get_member_value<SourceSymbol>(source);
+    ctti::map<SourceSymbol, SinkSymbol>(source, sink, ctti::default_symbol_mapping_function());
 }
 
-namespace detail
+template<typename SourceSymbol, typename SinkSymbol, typename Function = default_symbol_mapping_function>
+struct symbol_mapping
 {
+    constexpr symbol_mapping(Function function) :
+        function{function}
+    {}
 
-template<typename SourceSymbol, typename SinkSymbol>
-struct symbol_map_pair
-{
-    constexpr symbol_map_pair() = default;
-    constexpr symbol_map_pair(SourceSymbol, SinkSymbol) {}
+    Function function;
 };
 
+template<typename SourceSymbol, typename SinkSymbol, typename Function>
+constexpr ctti::symbol_mapping<SourceSymbol, SinkSymbol, ctti::meta::decay_t<Function>> mapping(const Function& function)
+{
+    return {function};
 }
 
 template<typename SourceSymbol, typename SinkSymbol>
-constexpr ctti::detail::symbol_map_pair<SourceSymbol, SinkSymbol> mapping()
+constexpr ctti::symbol_mapping<SourceSymbol, SinkSymbol> mapping()
 {
-    return {};
+    return {ctti::default_symbol_mapping_function()};
 }
 
-template<typename Source, typename Sink, typename... SourceSymbols, typename... SinkSymbols>
-constexpr void map(const Source& source, Sink& sink, ctti::detail::symbol_map_pair<SourceSymbols, SinkSymbols>...)
+template<typename Source, typename Sink, typename... SourceSymbols, typename... SinkSymbols, typename... MappingFunctions>
+constexpr void map(const Source& source, Sink& sink, const ctti::symbol_mapping<SourceSymbols, SinkSymbols, MappingFunctions>&... mapping)
 {
-    [](...){}((map<SourceSymbols, SinkSymbols>(source, sink), 0)...);
+    [](...){}((map<SourceSymbols, SinkSymbols>(source, sink, mapping.function), 0)...);
 }
 
 }
--- tests/map.cpp
+++ tests/map.cpp
@@ -1,4 +1,5 @@
 #include <ctti/map.hpp>
+#include <sstream>
 #include "catch.hpp"
 
 struct Foo
@@ -18,6 +19,41 @@ CTTI_DEFINE_SYMBOL(str);
 CTTI_DEFINE_SYMBOL(j);
 CTTI_DEFINE_SYMBOL(anotherString);
 
+template<typename T>
+std::string lexical_cast(const T& value)
+{
+    std::ostringstream ss;
+    ss << value;
+    return ss.str();
+}
+
+template<typename T>
+T lexical_cast(const std::string& str)
+{
+    std::istringstream ss{str};
+    T value;
+    ss >> value;
+    return value;
+}
+
+struct string_to_value
+{
+    template<typename Source, typename SourceSymbol, typename Sink, typename SinkSymbol>
+    constexpr void operator()(const Source& source, SourceSymbol, Sink& sink, SinkSymbol) const
+    {
+        ctti::get_member_value<SinkSymbol>(sink) = lexical_cast<typename SinkSymbol::template value_type<Sink>>(ctti::get_member_value<SourceSymbol>(source));
+    }
+};
+
+struct value_to_string
+{
+    template<typename Source, typename SourceSymbol, typename Sink, typename SinkSymbol>
+    constexpr void operator()(const Source& source, SourceSymbol, Sink& sink, SinkSymbol) const
+    {
+        ctti::get_member_value<SinkSymbol>(sink) = lexical_cast(ctti::get_member_value<SourceSymbol>(source));
+    }
+};
+
 TEST_CASE("map")
 {
     Foo foo{42, "foo"};
@@ -27,4 +63,11 @@ TEST_CASE("map")
 
     REQUIRE(bar.j == 42);
     REQUIRE(bar.anotherString == "foo");
+
+    foo.str = "42";
+    foo.i = 0;
+
+    ctti::map(foo, bar, ctti::mapping<i, anotherString>(value_to_string()), ctti::mapping<str, j>(string_to_value()));
+    REQUIRE(bar.anotherString == "0");
+    REQUIRE(bar.j == 42);
 }
